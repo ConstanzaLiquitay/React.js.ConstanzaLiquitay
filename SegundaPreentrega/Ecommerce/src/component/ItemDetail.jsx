8ddde750c7c5b231dfa065c3260de7b892585fd5
Constanza <cotyliquitay@gmail.com>
2023-08-29 18:02:49 -0300

    Nuevo commit

--- SegundaPreentrega/Ecommerce/src/component/ItemDetail.jsx
+++ SegundaPreentrega/Ecommerce/src/component/ItemDetail.jsx
@@ -1,6 +1,22 @@
-const ItemDetail = () => {
-  return <div>ItemDetail</div>
+import PropTypes from "prop-types";
 
-}
+const ItemDetail = ({item}) => {
+    if(!item){
+        return null;
+
+    };
+
+
+    return (
+        <div>
+            <h1>{item.title}</h1>
+            <h2>{item.category}</h2>
+        </div>
+    );
+};
+
+ItemDetail.propTypes = {
+    item: PropTypes.object,
+};
 
 export default ItemDetail
--- SegundaPreentrega/Ecommerce/src/component/ItemDetailContainer.jsx
+++ SegundaPreentrega/Ecommerce/src/component/ItemDetailContainer.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getProduct } from "../services";
+import ItemDetail from "./ItemDetail";
+
+
 const ItemDetailContainer = () => {
-  return <div>ItemDetailContainer</div>
+  const [item, setItem] = useState(null);
+  const { id }= useParams();
   
-}
+  
+useEffect (() => {
+  getProduct(id)
+  .then((response) => {
+    setItem(response);
+  })
+
+  .catch(() => {
+    setItem(null);
+  });
+}, [id]);
+
+
+
+return <ItemDetail item={item}/>
+};
 
 export default ItemDetailContainer
